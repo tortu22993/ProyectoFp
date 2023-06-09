e1bebeb711dc9d6b2d4e5fa1dc44b93ad90b56b5
tortu22993 <enrique.escalante@hotmail.es>
2023-06-09 14:51:32 +0200

    UbicacionUsuario
    Marker en la ubicacion del usuario

--- ProyectoFP-Enrique/app/src/main/java/com/example/proyectofp/pantallas/Urgencias.java
+++ ProyectoFP-Enrique/app/src/main/java/com/example/proyectofp/pantallas/Urgencias.java
@@ -1,11 +1,25 @@
 package com.example.proyectofp.pantallas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.example.proyectofp.R;
 import com.example.proyectofp.clasespojo.Pacientes;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.content.Intent;
 import android.location.Address;
@@ -27,6 +41,13 @@ public class Urgencias extends AppCompatActivity {
     private View buttonSearch;
     private ImageView homeBoton;
 
+    private GoogleMap googleMap;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +56,14 @@ public class Urgencias extends AppCompatActivity {
         homeBoton = findViewById(R.id.homeBoton);
         editTextSearch = findViewById(R.id.buscarEditText);
         buttonSearch = findViewById(R.id.buscarBoton);
+
+        // Verificar si el permiso ACCESS_FINE_LOCATION ha sido otorgado
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Si el permiso no ha sido otorgado, solicitarlo al usuario
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+
         homeBoton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,6 +78,27 @@ public class Urgencias extends AppCompatActivity {
                 String searchTerm = editTextSearch.getText().toString();
                 if (!searchTerm.isEmpty()) {
                     searchNearbyHospitals(searchTerm);
+                }
+            }
+        });
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // Habilitar el indicador de ubicación del dispositivo
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    googleMap.setMyLocationEnabled(true);
+                }
+
+                // Obtener la ubicación actual
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                // Añadir un marcador en la ubicación actual
+                if (location != null) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    googleMap.addMarker(new MarkerOptions().position(latLng).title("Mi ubicación"));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
                 }
             }
         });
@@ -73,4 +123,6 @@ public class Urgencias extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
+
 }
